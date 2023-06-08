590a1178d73931581acb65421f5ac5028145c640
Blastervasthunnadu14 <134245323+Blastervasthunnadu14@users.noreply.github.com>
2023-06-08 11:32:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ echo "Cloning Repo...."
 git clone https://github.com/Blastervasthunnadu14/UPLOADEDR24 /UPLOADER-BOT-V3
 cd /UPLOADER-BOT-V3
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @Blaster_Originals"
 python3 bot.py
